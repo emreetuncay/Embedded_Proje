88f785608f26ae04d3d7721f20702937fa4424fd
emreetuncay <ahmetemretuncay@gmail.com>
2022-12-02 14:26:01 +0300

    0-9 Display Sayacı
    Bu projede bir displayin 3 buton eşliğinde ileri geri ve sıfırlama durumunu inceledik

---
+++ Proje/main.c
@@ -0,0 +1,38 @@
+#include <main.h>
+#fuses XT,NOWDT,NOPROTECT,NOBROWNOUT,NOLVP,NOPUT,NOWRT,NODEBUG,NOCPD
+#use delay(clock=4000000)
+
+void main()
+{
+   char dizi[]= {63,6,91,79,102,125,7,127,111};
+   int x=0;
+   set_tris_b(0b00000111);
+   set_tris_c(0b00000000);
+   output_c(0b00000000);
+   output_c(dizi[0]); //Ekran 0 baþlasýn
+
+
+
+   for(;;)
+   {
+     if(input(pin_b0) == 1) // artýrma butonu
+    {
+         x++;
+         output_c(dizi[x]);
+         delay_ms(500);
+    }
+     if(input(pin_b1) == 1) // azaltma butonu
+    {
+         x--;
+         output_c(dizi[x]);
+         delay_ms(500);
+    }
+     if(input(pin_b2) == 1) // sýfýrla butonu
+    {
+         x=0;
+         output_c(dizi[0]);
+        
+    }
+   }
+
+}
